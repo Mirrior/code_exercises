8ffc028c2d647b6fdd8cafc52d986e8e27151b19
Rustin Player <rjplayer@outlook.com>
2018-06-26 11:37:44 -0600

    Fixed one of the methods and the tests

--- ruby_exercises/fraction_calculator.rb
+++ ruby_exercises/fraction_calculator.rb
@@ -1,12 +1,14 @@
 require 'rspec'
-# my way
+# my orginal way
 def fraction_calculator fraction_one, fraction_two, operator
   [fraction_one.to_r, fraction_two.to_r].reduce(operator.to_sym).to_s
 end
 
+# A better way
 def second_fraction_calculator operator, *fractions
-  [*fractions.map{|fraction| fraction.to_r}].reduce(operator.to_sym).to_s
+  fractions.map{|fraction| fraction.to_r}.reduce(operator.to_sym).to_s
 end
+
 # my way rewritten
 # def fraction_calculator fraction_one, fraction_two, operator
 #   fracs = [fraction_one.to_r, fraction_two.to_r]
@@ -14,10 +16,27 @@ end
 # end
 
 describe 'Fraction multiplication' do
-  it 'can multiply two fractions and output the value in a string based fraction format' do
-    expect(fraction_calculator("1/3", "2/4", "*")).to eq("1/6")
-    expect(fraction_calculator("1/3", "2/4", "/")).to eq("2/3")
-    expect(fraction_calculator("1/3", "2/4", "+")).to eq("5/6")
-    expect(fraction_calculator("1/3", "2/4", "-")).to eq("-1/6")
+  describe 'Way one' do
+    it 'can multiply two fractions and output the value in a string based fraction format' do
+      expect(fraction_calculator("1/3", "2/4", "*")).to eq("1/6")
+    end
+
+    it 'can divide two fractions and output the value in a string based fraction format' do
+      expect(fraction_calculator("1/3", "2/4", "/")).to eq("2/3")
+    end
+
+    it 'can add two fractions and output the value in a string based fraction format' do
+      expect(fraction_calculator("1/3", "2/4", "+")).to eq("5/6")
+    end
+
+    it 'can subtract two fractions and output the value in a string based fraction format' do
+      expect(fraction_calculator("1/3", "2/4", "-")).to eq("-1/6")
+    end
+  end
+
+  describe "Way two" do
+    it 'can add two fractions and output the value in a string based fraction format' do
+      expect(second_fraction_calculator("+", "1/3", "2/4")).to eq("5/6")
+    end
   end
 end
