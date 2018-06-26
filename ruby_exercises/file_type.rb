5d94176da2dbb09cf97dcf76990f5e407af141f4
Rustin Player <rjplayer@outlook.com>
2018-06-26 11:43:22 -0600

    Removed comments

--- ruby_exercises/file_type.rb
+++ ruby_exercises/file_type.rb
@@ -1,6 +1,5 @@
 require 'rspec'
 
-# Almos there
 class Array
   def file_selector type
     select{ |file| file.match(/\.#{type}$/)}.map do |file|
@@ -8,6 +7,7 @@ class Array
     end
   end
 end
+
 describe 'File selector' do
   it 'returns the ruby file names from an array of file names' do
     file_names = %w{file1.rb file2.html file3.rb file4.rb file5.js}
