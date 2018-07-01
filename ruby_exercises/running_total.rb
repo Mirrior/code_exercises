048935a8e9402cb55d94b042c0e913ffb9832baf
Rustin Player <rjplayer@outlook.com>
2018-07-01 16:35:53 -0600

    Rewrote method and explored different data structure for this method

--- ruby_exercises/running_total.rb
+++ ruby_exercises/running_total.rb
@@ -1,19 +1,46 @@
 require 'numbers_in_words'
 require 'numbers_in_words/duck_punch'
 
-@arr = []
+# using arrays
 
-def total
+def total arr = []
   puts 'enter exit if you would like to exit'
   puts 'else enter a new number to add'
 
   new_num = gets.chomp
-  abort("I am sad to see you go") if new_num =~ /[eE][xX][iI][tT]/
+  abort("I am sad to see you go") if new_num.casecmp?("exit")
 
-  @arr << new_num.in_numbers
-  puts "Total is #{@arr.inject(:+)}"
+  arr.push new_num.in_numbers
+  puts "Total is #{current_total = arr.inject(:+)}"
 
-  total
+  total [current_total]
 end
 
-total
+# @arr = []
+
+# def first_total_method
+#   puts 'enter exit if you would like to exit'
+#   puts 'else enter a new number to add'
+
+#   new_num = gets.chomp
+#   abort("I am sad to see you go") if new_num.casecmp?("exit")
+
+#   @arr << new_num.in_numbers
+#   puts "Total is #{@arr.inject(:+)}"
+
+#   different_total_method
+# end
+
+def new_total_method int = 0
+  puts 'enter exit if you would like to exit'
+  puts 'otherwise enter a new number to add'
+  
+  new_num = gets.chomp
+  abort("I am sad to see you go") if new_num.casecmp?("exit")
+
+  puts "Total is #{int += new_num.in_numbers}"
+
+  new_total_method int
+end
+
+new_total_method
