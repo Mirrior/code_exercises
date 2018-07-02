8852e81034c0385d2fcfe167a677369875aa3183
Rustin Player <rjplayer@outlook.com>
2018-07-02 13:25:54 -0600

    Added a second isolated class for refinements

--- ruby_exercises/Refinements_in_Ruby.rb
+++ ruby_exercises/Refinements_in_Ruby.rb
@@ -8,21 +8,48 @@ module RubyComments
   end
 end
 
-class ContentController
-  using RubyComments
-  
-  def initialize(word)
-    @word = word
-  end
-
-  def hidden_content
-    @word.commentize
+module HTMLComments
+  refine String do
+    def commentize
+      "<!-- #{self} -->"
+    end
   end
 end
 
+
+class RubyContentController
+  using RubyComments
+
+    def initialize(word)
+      @word = word
+    end
+
+    def hidden_content
+      @word.commentize
+    end
+end
+
+class HTMLContentController
+  using HTMLComments
+
+    def initialize(word)
+      @word = word
+    end
+
+    def hidden_content
+      @word.commentize
+    end
+end
+
+
 describe 'Refining Strings for a specific module' do
   it 'changes the behavior of the String method to make it render as a comment' do
-    cc = ContentController.new("My String")
-    expect(cc.hidden_content).to eq("# My String")
+    cc = RubyContentController.new("My Ruby String")
+    expect(cc.hidden_content).to eq("# My Ruby String")
+  end
+
+  it 'changes the behavior of the String method to make it render as a comment' do
+    html = HTMLContentController.new("My HTML String")
+    expect(html.hidden_content).to eq("<!-- My HTML String -->")
   end
 end
