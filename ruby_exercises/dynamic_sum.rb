59c15818ba43c6a84ad9442c12051037c314f712
Rustin Player <rjplayer@outlook.com>
2018-07-03 00:59:06 -0600

    New code challenge

---
+++ ruby_exercises/dynamic_sum.rb
@@ -0,0 +1,11 @@
+require 'rspec'
+
+def sum_generator num
+  (1..num).reduce(0, :+)
+end
+
+describe 'Dynamic sum' do
+  it 'Outputs a sum of digits, ranging from 0 through the argument value' do
+    expect(sum_generator 10).to eq(55)
+  end
+end
