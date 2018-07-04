32e42d5da7604c8b84d81ee7642c29c30420350b
Rustin Player <rjplayer@outlook.com>
2018-07-03 18:05:29 -0600

    Challenge to check if Value Exists in a Set of Nested Hashes

---
+++ ruby_exercises/Jan20.rb
@@ -0,0 +1,25 @@
+require 'rspec'
+
+class Array
+  def value_included? value
+    map{|hash| hash.values }.flatten.include? value
+  end
+end
+
+describe 'Collection search' do
+  it 'checks to see if a value is included in any number of hashes nested inside an array' do
+    books = [
+      {
+        :title => 'Fountainhead',
+        :author => 'Ayn Rand'
+      },
+      {
+        :title => 'Deep Work',
+        :author => 'Cal Newport'
+      }
+    ]
+
+    expect(books.value_included? 'Deep Work').to eq(true)
+    expect(books.value_included? 'Something Else').to eq(false)
+  end
+end
