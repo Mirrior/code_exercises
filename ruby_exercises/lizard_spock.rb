b106b3fe6d3e0b5d8ed2013342ffad991bc2e631
Rustin Player <rjplayer@outlook.com>
2018-08-08 13:17:27 -0600

    Changed Regex to be simplier

--- ruby_exercises/lizard_spock.rb
+++ ruby_exercises/lizard_spock.rb
@@ -1,19 +1,19 @@
 choices = %w(lizard spock scissors paper rock)
 p "your choices are: #{choices.join(", ")}"
-input = gets.chomp
+player_choice = gets.chomp
 
-com_input = choices.sample
-p com_input
+com_choice = choices.sample
+p com_choice
 
 another_array = choices.select{ |element| choices.find_index(element).odd? }
-choices.delete input
-win_lose_array = choices.select{ |element| choices.find_index(element).odd? }
-win_lose = win_lose_array.include? com_input
+choices.delete player_choice
+won_lost_choices = choices.select{ |element| choices.find_index(element).odd? }
+win_lose = won_lost_choices.include? com_choice
 
-case input
-when com_input
+case player_choice
+when com_choice
   p "tie"
-when /#{another_array.join("|")}/
+when /[#{another_array.join}]/
   if win_lose
     p "Win"
   else
